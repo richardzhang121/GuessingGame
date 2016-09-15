661e15d4d590d1b6a0932da7234a6b1a575d7c1b
Richard Zhang <269006@beaverton.k12.or.us>
2016-09-15 13:41:17 -0700

    Hopefully Last Commit...

--- Turnin/ZhangGuessing.cpp
+++ Turnin/ZhangGuessing.cpp
@@ -13,26 +13,27 @@ int main()
   int guess; //user's guess
   int counter = 0; //counts the number of terms t
   char input[5];
-  bool stillPlaying = true;
+  bool stillPlaying = true; //sets the boolean variable to be true
   
+  //while playing is true
   while (stillPlaying == true) {
-    initialGuess = rand() % 101;
-    while (initialGuess != guess) {
+    initialGuess = rand() % 101; //creates a random guess in between 0 and 100
+    while (initialGuess != guess) { //if the created guess was not equal
     cout << "Please enter a number between 0 and 100" << endl;
-    cin >> guess;
-      if (guess < 0 || guess > 100) {
+    cin >> guess; //takes in user input and registers as guess
+      if (guess < 0 || guess > 100) { //if the guess is less than 0 or greater than 100
         cout << "Please enter a number between 0 and 100" << endl;
-        counter++;
+        counter++; //adds guess to counter
       }
-      else if(guess < initialGuess) {
+      else if(guess < initialGuess) { //if guess was less than random number
         cout << "Number entered is too low" << endl;
         counter++;
       } 
-      else if (guess > initialGuess) {
+      else if (guess > initialGuess) { //if guess was greater than random number
         cout << "Number entered is too high" << endl;
         counter++;
       }
-      if (guess == initialGuess) {
+      if (guess == initialGuess) { //if guess was equal to guess
         cout << "Number is correct!" << endl;
         counter++;
         cout << "Number of Tries:" << counter << endl;
@@ -40,10 +41,10 @@ int main()
         cin >> input;
         if (input[0] == 'Y') {
           stillPlaying = true;
-          guess = -1;
+          guess = -1; //changes guess value, reintializes the game
         }
         else {
-          stillPlaying = false;
+          stillPlaying = false; //if false, exit game
         }
       }
     }
